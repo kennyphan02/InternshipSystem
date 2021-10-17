dd4270df89ab3f1be586c46b76faad61ef7c13e2
Kenny Phan <kenobusiness1@gmail.com>
2021-10-17 14:27:07 -0400

    Filled out user class. Added stud methods and return values

--- User.java
+++ User.java
@@ -1,8 +1,12 @@
 public abstract class User {
-    public User(){}
 
-    
-
-
-    
+    public ArrayList<Internship> searchInternships(ArrayList<Internship> internships){
+        return internships;
+    }
+    public ArrayList<Internship> searchInternships(ArrayList<Internship> internships, String keywords){
+        return internships;
+    }
+    public ArrayList<Internship> searchInternships(ArrayList<Internship> internships, Internship filters){
+        return internships;
+    }
 }
