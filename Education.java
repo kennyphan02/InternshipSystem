a3635cc3803965362d96d11c199769e89226349c
Kenny Phan <kenobusiness1@gmail.com>
2021-10-17 19:03:02 -0400

    added changes

--- Education.java
+++ Education.java
@@ -1,6 +1,10 @@
 public class Education {
     private String degree;
     private ArrayList<String> description;
+    /**
+     * Default constructor that initalizes the degree and description/
+     * @param degree a String being initalized
+     */
     public Education(String degree){
         this.degree = degree;
         description = new ArrayList<String>();
