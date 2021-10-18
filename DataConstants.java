e32692c955d69198a4b26cfd987a9ce0ecb20e67
danaiangelidis <angelidisdanai@gmail.com>
2021-10-17 21:21:13 -0400

    constants and loader

---
+++ DataConstants.java
@@ -0,0 +1,13 @@
+
+public class DataConstants {
+
+    protected static final String USER_ID = "user";
+    protected static final String PASSWORD = "pass";
+    protected static final String EMAIL = "email";
+    protected static final String BIRTH_DATE = "birthday";
+    protected static final String COUNTRY = "country";
+    protected static final String RESUME = "resume";
+    protected static final String COLLEGE_EDUCATION = "college";
+    protected static final String LANGUAGE = "language";
+
+}
---
+++ DataLoader.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class DataLoader extends DataConstants {
+
+    public ArrayList<User> getUsers() {
+        ArrayList<User> users = new ArrayList<User>();
+        return users;
+    }
+
+    public ArrayList<Internship> gInternships() {
+        ArrayList<Internship> internships = new ArrayList<Internship>();
+        return internships;
+    }
+
+}
