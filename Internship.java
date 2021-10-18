f1b60a10f1690f5dc54a0bde371fd01ea45481ae
Danielthewin <Danielthewin12@gmail.com>
2021-10-17 20:10:56 -0400

    Added methods and classes

---
+++ Internship.java
@@ -0,0 +1,31 @@
+public class Internship {
+    private String job;
+    private String degree;
+    private String partTime;
+    private String fullTime;
+    private String remote;
+    private String inPerson;
+    private int pay;
+    private int hours;
+    private Organization organization;
+    private String experience;
+    private Internship internship;
+    public Internship(String job, String degree, String partTime, String fullTime, 
+        String remote, String inPerson, int pay, int hours, Organization organization, 
+        String Experience, Internship internship) {
+            this.job = job;
+            this.degree = degree;
+            this.partTime = partTime;
+            this.fullTime = fullTime;
+            this.remote = remote;
+            this.inPerson = inPerson;
+            this.pay = pay;
+            this.hours = hours;
+            this.organization = organization;
+            this.experience = experience;
+            this.internship = internship;
+        }
+    public String toString() {
+        return "";
+    }
+}
