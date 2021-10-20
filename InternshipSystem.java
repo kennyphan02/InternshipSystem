b8a2b0dd601753fd0757d2fee72a23d5d908bbf8
Kenny Phan <kenobusiness1@gmail.com>
2021-10-20 11:24:23 -0400

    Added private helper methods, finished getting user input for experiences, improved code readability

--- InternshipSystem.java
+++ InternshipSystem.java
@@ -4,11 +4,24 @@ public class InternshipSystem {
     private InternshipList internshipList;
     private UserList userList;
     private Scanner scanner;
+    private String username;
+    private String password;
+    private String email;
+    private String birthday;
+    private String country;
+    private String firstName;
+    private String lastName;
+    private String profession;
+    private String college;
+    private Experience experience;
+    private Resume resume;
+
 
     public InternshipSystem(){
         // internshipList = InternshipList.getInstance();
         // userList = UserList.getInstance();
         scanner = new Scanner(System.in);
+        experience = new Experience();
     }
     
     public void login(){
@@ -33,53 +46,74 @@ public class InternshipSystem {
             System.out.flush();
             System.out.println("--------Internship System--------");
             System.out.print("Input username: ");
-            String username = scanner.nextLine();
+            username = scanner.nextLine();
             System.out.print("Input password: ");
-            String password = scanner.nextLine();
+            password = scanner.nextLine();
             System.out.print("Input email: ");
-            String email = scanner.nextLine();
+            email = scanner.nextLine();
             System.out.print("Input birthday: ");
-            String birthday = scanner.nextLine();
+            birthday = scanner.nextLine();
             System.out.print("Input country: ");
-            String country = scanner.nextLine();
+            country = scanner.nextLine();
             System.out.println("");
-            System.out.println("Create your student profile that will be displayed to organizations: ");
-            System.out.println("");
-            System.out.print("First name: ");
-            String firstName = scanner.nextLine();
-            System.out.print("Last name: ");
-            String lastName = scanner.nextLine();
-            System.out.println("Email: " + email);
-            System.out.println("Birthday: " + birthday);
-            System.out.println("Country: " + country);
-            System.out.print("College: ");
-            String college = scanner.nextLine();
-            System.out.println("");
-            System.out.println("Add the following details to your resume:");
-            System.out.println("");
-            System.out.println("First Name: " + firstName);
-            System.out.println("Last name: " + lastName);
-            System.out.print("Profession: ");
-            String profession = scanner.nextLine();
-            System.out.println("Email: " + email);
-            System.out.println("");
-            System.out.println("Input experience. Enter 1 to stop adding experiences");
-            System.out.println("");
-            System.out.println("Title: ");
-            boolean count = true;
-            Experience experience = new Experience();
-            
-            while(count){
-                System.out.print("Title: ");
-                String title = scanner.nextLine();
-
-            
-            }
-
-
-
-            
+            createStudentProfile();
+            createResume();    
         }
+    }
+
+    private void createStudentProfile(){
+        System.out.println("Create your student profile that will be displayed to organizations: ");
+        System.out.println("");
+        System.out.print("First name: ");
+        firstName = scanner.nextLine();
+        System.out.print("Last name: ");
+        lastName = scanner.nextLine();
+        System.out.println("Email: " + email);
+        System.out.println("Birthday: " + birthday);
+        System.out.println("Country: " + country);
+        System.out.print("College: ");
+        college = scanner.nextLine();
+        System.out.println("");
+
+    }
+
+    private void createResume(){
+        System.out.println("Add the following details to your resume:");
+        System.out.println("");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last name: " + lastName);
+        System.out.print("Profession: ");
+        profession = scanner.nextLine();
+        System.out.println("Email: " + email);
+        System.out.println("");
+        createExperiences();
+        createEducation();
+    }
+
+    private void createExperiences(){
+        System.out.println("Input experience. Enter \"quit\" before adding an experience to quit");
+        System.out.println("");
+        boolean count = true; 
+        while(count){
+            System.out.print("Input title:  ");
+            String title = scanner.nextLine();
+            experience.addTitle(title);
+            System.out.print("Input description:");
+            String description = scanner.nextLine();
+            experience.addDescription(description);
+            System.out.print("Input quit? type quit to quit or no to keep inputting experiences: ");
+            String quit = scanner.nextLine();
+            if(quit.equals("quit")){
+                count = false;
+            } else if(quit.equals("no")){
+                continue;
+            }
+        }
+    }
+
+    private void createEducation(){
+        System.out.println("");
+
     }
 
     public int getUserInput(){
