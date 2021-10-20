0fb66ea4402ef51f9d84a59df68bce47434cf0e0
Kenny Phan <kenobusiness1@gmail.com>
2021-10-20 00:05:25 -0400

    Modified UserList singleton, created getInstance method

--- UserList.java
+++ UserList.java
@@ -1,3 +1,13 @@
 public class UserList {
-    
+    private ArrayList<User> userList;
+    private static UserList users;
+    private UserList() {
+
+    }
+    public static UserList getInstance(){
+        if(users == null){
+            users = new UserList();
+        }
+        return users;
+    }
 }
