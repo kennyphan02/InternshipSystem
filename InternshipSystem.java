4f2f454720f63abee833e9c3ed029931ad88fd5c
Kenny Phan <kenobusiness1@gmail.com>
2021-10-19 23:12:51 -0400

    Added private variables and created default constructor for InternshipSystem.java

--- InternshipSystem.java
+++ InternshipSystem.java
@@ -1,3 +1,23 @@
 public class InternshipSystem {
+    private Organization organization;
+    private Student student;
+    private Internship internship;
+    private InternshipList internshipList;
+    private User user;
+    private UserList userList;
+    public InternshipSystem(Organization organization, Student student, Internship internship, InternshipList internshipList, User user, UserList userList){
+        this.organizataion = organization;
+        this.student = student;
+        this.internship = internship;
+        this.internshipList = internshipList;
+        this.user = user;
+        this.userList = userList;
+    }
+
+
+
+
+
+
     
 }
