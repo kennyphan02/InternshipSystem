9412917000f2ffb681d976366bb462f32b59c642
danaiangelidis <angelidisdanai@gmail.com>
2021-10-26 12:47:34 -0400

    add organization methods to DataWriter.java

--- src/DataWriter.java
+++ src/DataWriter.java
@@ -30,9 +30,54 @@ public class DataWriter extends DataConstants {
 		studentDetails.put(STUDENT_UUID, student.getId().toString());
 		studentDetails.put(STUDENT_USER_NAME, student.getUser());
 		studentDetails.put(STUDENT_BIRTHDAY, student.getBirthday());
+        studentDetails.put(STUDENT_PASSWORD, student.getPassword());
+        studentDetails.put(STUDENT_EMAIL, student.getEmail());
+        studentDetails.put(STUDENT_BIRTHDAY, student.getBirthday());
+        studentDetails.put(STUDENT_COUNTRY, student.getCountry());
+        studentDetails.put(STUDENT_RESUME, student.getResume());
+        studentDetails.put(STUDENT_COLLEGE, student.getCollege());
+        studentDetails.put(STUDENT_LANGUAGES, student.getLanguage());
+        studentDetails.put(STUDENT_BOOKMARKS, student.getBookmarks());
+        studentDetails.put(STUDENT_REVIEWS, student.getReview());
         
         return studentDetails;
 	}
 
+    /**
+     * 
+     */
 
+    public static void saveOrganization() {
+		Organizations organizations = Organizations.getInstance();
+		ArrayList<Organization> orgList = organizations.getOrganizationsList();
+		JSONArray jsonOrganizations = new JSONArray();
+		
+		for(int i=0; i< orgList.size(); i++) {
+			jsonOrganizations.add(getOrganizationJSON(orgList.get(i)));
+		}
+		
+        try (FileWriter file = new FileWriter(ORG_FILE_NAME)) {
+ 
+            file.write(jsonOrganizations.toJSONString());
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static JSONObject getOrganizationJSON(Organization organization) {
+		JSONObject orgDetails = new JSONObject();
+		orgDetails.put(ORG_UUID, organization.getId().toString());
+		orgDetails.put(ORG_USERNAME, organization.getUser());
+        orgDetails.put(ORG_PASSWORD, organization.getPassword());
+        orgDetails.put(ORG_EMAIL, organization.getEmail());
+        orgDetails.put(ORG_COUNTRY, organization.getCountry());
+        orgDetails.put(ORG_NAME, organization.getOrgName());
+        orgDetails.put(ORG_PHONE_NUM, organization.getPhoneNumber());
+        orgDetails.put(ORG_DESCRIPTION, organization.getDesc());
+        orgDetails.put(ORG_INTERNSHIPS, organization.getInternships());
+        
+        return orgDetails;
+	}
 }
