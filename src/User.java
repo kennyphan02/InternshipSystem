57f3e419c17eebc46c54217ce28dd23caf0c502c
Danielthewin <Danielthewin12@gmail.com>
2021-10-26 12:15:37 -0400

    add User.java javadoc

--- src/User.java
+++ src/User.java
@@ -1,15 +1,29 @@
 import java.util.ArrayList;
 
 public class User {
-
+/**
+ * searches the internship array.
+ * @param internships type ArrayList<Internships>
+ * @return the internships found. 
+ */
     public ArrayList<Internship> searchInternships(ArrayList<Internship> internships) {
         return internships;
     }
-
+/**
+ * searches the internship array.
+ * @param internships type ArrayList<Internship>
+ * @param keywords type string
+ * @return the internships found.
+ */
     public ArrayList<Internship> searchInternships(ArrayList<Internship> internships, String keywords) {
         return internships;
     }
-
+/**
+ * searches the internship array.
+ * @param internships type ArrayList<Internship>
+ * @param filters type Internships
+ * @return the internships found.
+ */
     public ArrayList<Internship> searchInternships(ArrayList<Internship> internships, Internship filters) {
         return internships;
     }
