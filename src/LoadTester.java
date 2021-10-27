081092ea51355ffdcc8c43ca5683e6c00ecd8c0a
JacobL84 <JSUHS@email.sc.edu>
2021-10-26 20:20:33 -0400

    remove LoadTester.java

--- src/LoadTester.java
+++
@@ -1,7 +0,0 @@
-public class LoadTester {
-    public static void main(String[] args)  {
-        Resumes.getInstance();
-        System.out.println("Loaded Resumes Without Errors.");
-        Students.getInstance();    
-    }
-}
