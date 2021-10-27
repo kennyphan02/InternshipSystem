d2cd40166c20e89eb2e7061188a6bf732d059660
Danielthewin <Danielthewin12@gmail.com>
2021-10-27 12:03:21 -0400

    add Review.java javadoc

--- src/Review.java
+++ src/Review.java
@@ -19,6 +19,14 @@ public class Review {
         this.comment = comment;
         this.rating = rating;
     }
+
+    /**
+     * Default constructor that initalizes the private variables of the Review class.
+     * @param id type UUID
+     * @param title type String
+     * @param comment type String
+     * @param rating type int
+     */
     public Review(UUID id, String title, String comment, int rating) {
         this.id = id;
         this.title = title;
@@ -26,22 +34,41 @@ public class Review {
         this.rating = rating;
     }
 
+    /**
+     * prints out.
+     */
     public String toString() {
         return "";
     }
 
+    /**
+     * gets the id.
+     * @return the id of the student.
+     */
     public UUID getId() {
         return id;
     }
 
+    /**
+     * gets the title of the review.
+     * @return the title of the review. 
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * gets the comments of the reviews.
+     * @return the comments of the reviews posted. 
+     */
     public String getComment() {
         return comment;
     }
 
+    /**
+     * gets the rating posted. 
+     * @return the rating that was posted. 
+     */
     public int getRating() {
         return rating;
     }
