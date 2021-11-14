2140ef5e54ed2be2ab8ea213e48513fb22061bdf
Kenny Phan <kenobusiness1@gmail.com>
2021-11-14 16:24:17 -0500

    Add multiple test methods

--- src/InternshipSystemTest.java
+++ src/InternshipSystemTest.java
@@ -53,6 +53,14 @@ public class InternshipSystemTest {
     }
 
     @Test
+    public void testCreateValidOrganizationAccount(){
+        internshipSystem.createOrganizationAccount("HBOMax123", "samsungtv123", "hbo@gmail.com", "USA", "HBOMax", "821-341-1921", "tv stuff");
+        internshipSystem.login("HBOMax123", "samsungtv123");
+        Organization currentOrganization = internshipSystem.getOrganizationUser();
+        assertEquals("HBOMax123", currentOrganization.getUser());
+    }
+
+    @Test
     public void testCheckDuplicateUserName(){
         ArrayList<String> skills = new ArrayList<String>();
         ArrayList<String> language = new ArrayList<String>();
@@ -71,11 +79,76 @@ public class InternshipSystemTest {
         internshipSystem.createStudentAccount("tech123", "abc123", "bob@gmail.com", "6/18/2002", "USA", resume, "UofSC", language);
         boolean duplicateUserAccount = internshipSystem.createStudentAccount("tech123", "tere1123", "poo@gmail.com", "9/12/1994", "USA", resume, "UofSC", language);
         assertFalse(duplicateUserAccount);
-
     }
 
+    @Test
+    public void testCreateEmptyUserName(){
+        ArrayList<String> skills = new ArrayList<String>();
+        ArrayList<String> language = new ArrayList<String>();
+        ArrayList<Experience> experiences = new ArrayList<Experience>();
+        skills.add("Java");
+        skills.add("Python");
+        ArrayList<String> responsibilities = new ArrayList<String>();
+        responsibilities.add("Created a Python program");
+        responsibilities.add("Built java program");
+        Experience experience = new Experience("Computer Programmer", "techsters", "Aug 2019", "Oct 2020", responsibilities);
+        experiences.add(experience);
+        Education education = new Education("UofSC","Computer Science","5/12/2021");
+        language.add("English");
+        language.add("Russian");
+        Resume resume = new Resume(skills, experiences, education, language);
+        boolean invalidUser = internshipSystem.createStudentAccount("", "abc123", "bob@gmail.com", "6/18/2002", "USA", resume, "UofSC", language);
+        assertFalse(invalidUser);
+    }
 
+    @Test
+    public void testcreateEmptyAccount(){
+        boolean invalidUser = internshipSystem.createStudentAccount("", "", "", "", "", null, "", null);
+        assertFalse(invalidUser);
+    }
 
+    @Test
+    public void testValidLogin(){
+        ArrayList<String> skills = new ArrayList<String>();
+        ArrayList<String> language = new ArrayList<String>();
+        ArrayList<Experience> experiences = new ArrayList<Experience>();
+        skills.add("Java");
+        skills.add("Python");
+        ArrayList<String> responsibilities = new ArrayList<String>();
+        responsibilities.add("Created a Python program");
+        responsibilities.add("Built java program");
+        Experience experience = new Experience("Computer Programmer", "techsters", "Aug 2019", "Oct 2020", responsibilities);
+        experiences.add(experience);
+        Education education = new Education("UofSC","Computer Science","5/12/2021");
+        language.add("English");
+        language.add("Russian");
+        Resume resume = new Resume(skills, experiences, education, language);
+        internshipSystem.createStudentAccount("tech123", "abc123", "bob@gmail.com", "6/18/2002", "USA", resume, "UofSC", language);
 
-    
+        boolean validLogin = internshipSystem.login("tech123", "abc123");
+        assertTrue(validLogin);
+    }
+
+    @Test
+    public void testInvalidLogin(){
+        ArrayList<String> skills = new ArrayList<String>();
+        ArrayList<String> language = new ArrayList<String>();
+        ArrayList<Experience> experiences = new ArrayList<Experience>();
+        skills.add("Java");
+        skills.add("Python");
+        ArrayList<String> responsibilities = new ArrayList<String>();
+        responsibilities.add("Created a Python program");
+        responsibilities.add("Built java program");
+        Experience experience = new Experience("Computer Programmer", "techsters", "Aug 2019", "Oct 2020", responsibilities);
+        experiences.add(experience);
+        Education education = new Education("UofSC","Computer Science","5/12/2021");
+        language.add("English");
+        language.add("Russian");
+        Resume resume = new Resume(skills, experiences, education, language);
+        internshipSystem.createStudentAccount("tech123", "abc123", "bob@gmail.com", "6/18/2002", "USA", resume, "UofSC", language);
+
+        boolean validLogin = internshipSystem.login("tech123", "abcdsgfdh123");
+        assertFalse(validLogin);
+
+    }
 }
